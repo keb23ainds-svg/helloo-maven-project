aa4ebf721aae05c0788397f184e7fa7935adfd74
keerthana <keb23ainds@cmrit.ac.in>
2026-05-13 06:41:55 +0530

    Initial Maven project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Azure Pipeline");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
